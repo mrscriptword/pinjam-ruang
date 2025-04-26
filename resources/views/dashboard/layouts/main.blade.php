c3fc577c06ab5da8bdc094b687291d8c896af135
yolwuldar <yolandawulri@gmail.com>
2025-04-26 12:24:36 +0700

    benerin logo halaman admin

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
     <title>{{ $title }} | Universitas Sultan Ageng Tirtayasa</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/UNTIRTA.webp') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/Logo_UNTIRTA') }}">
 </head>
 
 <body>
